91d7a474b64d16ac27646ee01e69a29d98cf974b
Sergey Kochegarov <sergey@designmix.ru>
2019-12-09 14:07:20 +0300

    Create gendiff --format

--- src/Engine.php
+++ src/Engine.php
@@ -12,14 +12,19 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]  
 
 DOCOPT;
 
-    $result = Docopt::handle($doc, ['version' => '1.0.0rc2']);
+    $result = Docopt::handle($doc, [
+        'version' => '0.1',
+
+    ]);
     foreach ($result as $k => $v) {
         echo $k . ': ' . json_encode($v) . PHP_EOL;
     }

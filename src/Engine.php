a938c0b4d603bfee699ff76953980c1ef5c056b7
sergey <sergey@designmix.ru>
2020-01-06 17:30:57 +0300

    Реализация gendiff json
    Сравнение плоских файлов (JSON)

--- src/Engine.php
+++ src/Engine.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Calculator\Engine;
+namespace Differ;
 
 use Docopt;
+use Funct;
 
 function run()
 {
@@ -23,9 +24,63 @@ DOCOPT;
 
     $result = Docopt::handle($doc, [
         'version' => '0.1',
-
     ]);
-    foreach ($result as $k => $v) {
-        echo $k . ': ' . json_encode($v) . PHP_EOL;
+
+    if (array_key_exists('--format', $result->args)) {
+        $diff = genDiff($result->args['<firstFile>'], $result->args['<secondFile>']);
+        print_r($diff);
     }
+}
+
+function genDiff($pathToFile1, $pathToFile2)
+{
+    $diff = diffArray(getFromJson($pathToFile1), getFromJson($pathToFile2));
+    $result = implode(array_map(function ($item) {
+            return " {$item['diff']} {$item['key']}: {$item['value']}\n";
+        }, $diff));
+    return "{\n{$result}}";
+}
+
+function createAcc($diff, $key, $value)
+{
+    if ($value === true) $value = 'true';
+    if ($value === false) $value = 'false';
+    return [
+        'diff' => $diff,
+        'key' => $key,
+        'value' => $value,
+    ];
+}
+
+function diffArray($array1, $array2)
+{
+    $keys = array_keys(array_merge($array1, $array2));
+    $result = array_reduce($keys, function ($acc, $key) use ($array1, $array2) {
+        if (array_key_exists($key, $array1) && array_key_exists($key, $array2)) {
+            if ($array1[$key] == $array2[$key]) {
+                $acc[][] = createAcc(' ', $key, $array2[$key]);
+            } else {
+                $acc[] = [
+                    createAcc('+', $key, $array2[$key]),
+                    createAcc('-', $key, $array1[$key])
+                ];
+            }
+        } elseif (array_key_exists($key, $array1)) {
+            $acc[][] = createAcc('-', $key, $array1[$key]);
+        } elseif (array_key_exists($key, $array2)){
+            $acc[][] = createAcc('+', $key, $array2[$key]);
+        }
+
+        return $acc;
+    }, []);
+    return array_merge(...$result);
+}
+
+function getFromJson($pathToFile)
+{
+    return json_decode(
+        file_get_contents(
+            file_exists($pathToFile) ? $pathToFile : getcwd() . '/' . $pathToFile),
+            True
+    );
 }

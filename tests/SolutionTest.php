dd4d993ebe6c7dae7a49d09a5daa8e8227be2467
sergey <sergey@designmix.ru>
2020-01-11 13:13:22 +0300

    update README

---
+++ tests/SolutionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+    public function testName()
+    {
+        $this->assertTrue(True);
+        $this->assertFalse(False);
+    }
+}

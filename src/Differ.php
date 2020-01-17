6caa0d87aef710f1d46ebeebeded5bd25d291788
Sergey Kochegarov <sergey@designmix.ru>
2020-01-17 17:37:58 +0300

    adding tree structure

--- src/Differ.php
+++ src/Differ.php
@@ -6,11 +6,16 @@ use function DifferenceCalculator\Parsers\getData;
 
 function genDiff($pathToFile1, $pathToFile2)
 {
-    $diff = differ(getData($pathToFile1), getData($pathToFile2));
-    $result = implode(array_map(function ($item) {
-            return "  {$item['diff']} {$item['key']}: {$item['value']}\n";
+    $differData = differ(getData($pathToFile1), getData($pathToFile2));
+    $renderData = render($differData);
+    return "{\n{$renderData}}";
+}
+
+function render($diff)
+{
+    return implode(array_map(function ($item) {
+        return "  {$item['diff']} {$item['key']}: {$item['value']}\n";
     }, $diff));
-    return "{\n{$result}}";
 }
 
 function differ($array1, $array2)
@@ -19,17 +24,17 @@ function differ($array1, $array2)
     $result = array_reduce($keys, function ($acc, $key) use ($array1, $array2) {
         if (array_key_exists($key, $array1) && array_key_exists($key, $array2)) {
             if ($array1[$key] == $array2[$key]) {
-                $acc[][] = createAcc(' ', $key, $array2[$key]);
+                $acc[][] = createItem(' ', $key, $array2[$key]);
             } else {
                 $acc[] = [
-                    createAcc('+', $key, $array2[$key]),
-                    createAcc('-', $key, $array1[$key])
+                    createItem('+', $key, $array2[$key]),
+                    createItem('-', $key, $array1[$key])
                 ];
             }
         } elseif (array_key_exists($key, $array1)) {
-            $acc[][] = createAcc('-', $key, $array1[$key]);
+            $acc[][] = createItem('-', $key, $array1[$key]);
         } elseif (array_key_exists($key, $array2)) {
-            $acc[][] = createAcc('+', $key, $array2[$key]);
+            $acc[][] = createItem('+', $key, $array2[$key]);
         }
 
         return $acc;
@@ -37,7 +42,7 @@ function differ($array1, $array2)
     return array_merge(...$result);
 }
 
-function createAcc($diff, $key, $value)
+function createItem($diff, $key, $value)
 {
     if ($value === true) {
         $value = 'true';
--- tests/EngineTest.php
+++ tests/EngineTest.php
@@ -24,4 +24,23 @@ class EngineTest extends TestCase
         $diff = genDiff($pathBefore, $pathAfter);
         $this->assertEquals($resultTrue, $diff);
     }
+
+//    public function testTreeJson()
+//    {
+//        $pathBefore = __DIR__ . "/fixtures/before_tree.json";
+//        $pathAfter = __DIR__ . "/fixtures/after_tree.json";
+//        $resultTrue = file_get_contents(__DIR__ . "/fixtures/result_tree.true");
+//        $diff = genDiff($pathBefore, $pathAfter);
+//        $this->assertEquals($resultTrue, $diff);
+//    }
+//
+//    public function testTreeYaml()
+//    {
+//        $pathBefore = __DIR__ . "/fixtures/before_tree.yml";
+//        $pathAfter = __DIR__ . "/fixtures/after_tree.yml";
+//        $resultTrue = file_get_contents(__DIR__ . "/fixtures/result_tree.true");
+//        $diff = genDiff($pathBefore, $pathAfter);
+//        $this->assertEquals($resultTrue, $diff);
+//    }
+
 }
